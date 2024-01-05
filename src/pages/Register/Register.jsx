7b0c3bee15838cb90991eb75457bbff9493cba9a
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-05 20:13:18 +0100

    130: States do form de cadastro

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,29 +3,79 @@ import styles from "./Register.module.css";
 import { useState, useEffect } from "react";
 
 const Register = () => {
+  const [displayName, setDisplayName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setError("")
+
+    const user = {
+      displayName,
+      email,
+      password
+    }
+
+    if(password !== confirmPassword) {
+      setError("The chosen passwords doesn't match!")
+      return
+    }
+
+    console.log(user);
+  }
+
   return (
-    <div>
-        <h1>Register to post</h1>
-        <p>Register as a user and share your stories</p>
-        <form>
-          <label>
-            <span>Name:</span>
-            <input type="text" name="displayName" required placeholder="User full name" />
-          </label>
-          <label>
-            <span>E-mail:</span>
-            <input type="email" name="email" required placeholder="User e-mail" />
-          </label>
-          <label>
-            <span>Password:</span>
-            <input type="password" name="password" required placeholder="Type your password" />
-          </label>
-          <label>
-            <span>Password confirmation:</span>
-            <input type="password" name="confirmPassword" required placeholder="Confirm your password" />
-          </label>
-          <button className="btn">Register</button>
-        </form>
+    <div className={styles.register}>
+      <h1>Register to post</h1>
+      <p>Register as a user and share your stories</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Name:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="User full name"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)} />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input 
+          type="email" 
+          name="email" 
+          required 
+          placeholder="User e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input 
+          type="password" 
+          name="password" 
+          required 
+          placeholder="Type your password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}/>
+        </label>
+        <label>
+          <span>Password confirmation:</span>
+          <input 
+          type="password" 
+          name="confirmPassword" 
+          required 
+          placeholder="Confirm your password" 
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}/>
+        </label>
+        <button className="btn">Register</button>
+        {error && <p className="error">{error}</p>}
+      </form>
     </div>
   )
 }
