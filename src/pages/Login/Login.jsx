194fa27de8a74227c05a2585d35864cba824d267
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-06 18:42:28 +0100

    138: Estruturando a página de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,69 @@
 import styles from "./Login.module.css";
 
+import { useAuthentication } from "../../hooks/useAuthentication";
+
+import { useState, useEffect } from "react";
+
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("")
+
+    const user = {
+   
+      email,
+      password
+    }
+
+    const res = await createUser(user)
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError)
+    }
+  }, [authError])
   return (
-    <div>
-        <h2>Login</h2>
+    <div className={styles.login}>
+       <h1>Login</h1>
+      <p>Login to be able to interact</p>
+      <form onSubmit={handleSubmit}>
+        
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="User e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Type your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+        </label>
+       
+        {!loading && <button className="btn">Login</button>}
+        {loading &&  <button className="btn" disabled>Wait...</button>}
+        {error && <p className="error">{error}</p>}
+      </form>
     </div>
   )
 }
