ab423f918ffc155aed32c66e7317e4f71d37552b
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-06 18:17:50 +0100

    136: Bloqueando páginas que precisam de autenticação

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,14 @@
 // CSS
 import styles from "./About.module.css"
 
+import { Link } from "react-router-dom"
+
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+        <h2>About Mini <span>Blog</span></h2>
+        <p>This project consists of a blog made with React on the front-end and Firebase on the back-end</p>
+        <Link to="/posts/create" className="btn">Create post</Link>
     </div>
   )
 }
