f1ad9541bc57bd5b402d38c44a77ae872c06e470
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-09 16:54:13 +0100

    158: Atualizando o post

--- src/pages/EditPost/EditPost.jsx
+++ src/pages/EditPost/EditPost.jsx
@@ -3,8 +3,8 @@ import styles from "./EditPost.module.css";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
-import { useInsertDocument } from "../../hooks/useInsertDocument";
 import { useFetchDocument } from "../../hooks/useFetchDocument"
+import { useUpdateDocument } from "../../hooks/useUpdateDocument";
 
 const EditPost = () => {
     const { id } = useParams()
@@ -32,7 +32,7 @@ const EditPost = () => {
 
     const { user } = useAuthValue()
 
-    const { insertDocument, response } = useInsertDocument("posts")
+    const { updateDocument, response } = useUpdateDocument("posts")
 
     const navigate = useNavigate()
 
@@ -57,17 +57,19 @@ const EditPost = () => {
 
         if (formError) return;
 
-        insertDocument({
+        const data = {
             title,
             image,
             body,
             tagsArray,
             uid: user.uid,
             createdBy: user.displayName
-        })
+        }
+
+        updateDocument(id, data)
 
         // redirect to home page
-        navigate("/")
+        navigate("/dashboard")
     }
 
     return (
