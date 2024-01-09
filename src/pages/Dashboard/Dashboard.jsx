e6271219750a996e4d6485b8cb3a89345ab486db
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-09 12:57:21 +0100

    154: Concluindo estrutura da Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,37 +1,60 @@
 import styles from "./Dashboard.module.css";
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // hooks
-import {useAuthValue} from "../../context/AuthContext";
-import {useFetchDocuments} from "../../hooks/useFetchDocuments"
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments"
 
 const Dashboard = () => {
-  const {user} = useAuthValue()
+  const { user } = useAuthValue()
   const uid = user.uid
 
-  const {documents: posts, loading} = useFetchDocuments("posts", null, uid);
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const deleteDocument = (id) => {};
+
+  if(loading) {
+    return <p>Loading...</p>
+  }
 
   return (
     <div>
-        <h2>Dashboard</h2>
-        <p>Manage your posts</p>
-        {posts && posts.length === 0 ? (
-          <div className={styles.noposts}>
-            <p>No posts have been found</p>
-            <Link to="/posts/create" className="btn">
-              Create first post
-            </Link>
+      <h2>Dashboard</h2>
+      <p>Manage your posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>No posts have been found</p>
+          <Link to="/posts/create" className="btn">
+            Create first post
+          </Link>
+        </div>
+      ) : (
+        <>
+          <div>
+            <span>Title</span>
+            <span>Actions</span>
           </div>
-        ) : (
-          <div> 
-            <p>There are posts!</p>
-          </div>
-        )}
 
-        {posts && posts.map((post) => (
-          <h3>{post.title}</h3>
-        ))}
+          {posts && posts.map((post) => (
+            <div key={post.id}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                  See
+                </Link>
+                <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+                  Edit
+                </Link>
+                <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}</>
+      )}
+
+
     </div>
   )
 }
