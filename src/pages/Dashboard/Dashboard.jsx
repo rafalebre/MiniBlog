29a6e638d18db2912d08f1b8b256bfcd00dd14e8
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-09 11:57:08 +0100

    152: Estruturando Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,34 @@
 import styles from "./Dashboard.module.css";
 
+import {Link} from "react-router-dom";
+
+// hooks
+import {useAuthValue} from "../../context/AuthContext";
+import {useFetchDocuments} from "../../hooks/useFetchDocuments"
+
 const Dashboard = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+
+  // posts do usuário
+  const posts = []
+
   return (
     <div>
-        <h1>Dashboard</h1>
+        <h2>Dashboard</h2>
+        <p>Manage your posts</p>
+        {posts && posts.length === 0 ? (
+          <div className={styles.noposts}>
+            <p>No posts have been found</p>
+            <Link to="/posts/create" className="btn">
+              Create first post
+            </Link>
+          </div>
+        ) : (
+          <div> 
+            <p>There are posts!</p>
+          </div>
+        ) }
     </div>
   )
 }
